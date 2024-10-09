50d6ff2fa1fb30b4b88947c1c2861a949d7b15d4
rajeshchauhan1426 <codedemon1426@gmail.com>
2024-10-10 00:37:25 +0530

    error update

--- components/ui/bentogrid.tsx
+++ components/ui/bentogrid.tsx
@@ -26,20 +26,28 @@ export const BentoGridItem = ({
   header,
   icon,
   id,
+  img,
+  imgClassName,
+  titleClassName,
+  spareImg,
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
   header?: React.ReactNode;
   icon?: React.ReactNode;
-  id:number,
+  id:number;
+  img: string;
+  imgClassName: string;
+  titleClassName:string;
+  spareImg: string; 
 }) => {
   return (
     <div
       className={cn(
         "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
         className
-      )}
+       )}
     >
       {header}
       <div className="group-hover/bento:translate-x-2 transition duration-200">
