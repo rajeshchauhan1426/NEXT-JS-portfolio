043bad61942953dbe321fcf801a27a8f324cbc28
rajeshchauhan1426 <codedemon1426@gmail.com>
2024-10-10 00:45:11 +0530

    update

--- data/index.ts
+++ data/index.ts
@@ -20,7 +20,7 @@ export const navItems = [
       id: 2,
       title: "I'm very flexible with time zone communications",
       description: "",
-      className: "lg:col-span-2 md:col-span-3  lg:min-h-[40vh] md:row-span-2 h-15 ",
+      className: "lg:col-span-2 md:col-span-3  lg:min-h-[40vh] md:row-span-2 ",
       imgClassName: "",
       titleClassName: "justify-start",
       img: "",
