a9bb840414f3f13fd1c622d00bf3951e9b33e32c
rajeshchauhan1426 <codedemon1426@gmail.com>
2024-10-22 13:47:11 +0530

    css and global update

--- components/ui/bentogrid.tsx
+++ components/ui/bentogrid.tsx
@@ -49,7 +49,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 relative rounded-3xl  overflow-hidden group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
         className
        )}
        style={{
@@ -90,10 +90,24 @@ export const BentoGridItem = ({
           <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
             {title}
           </div>
-        </div>
+        
         {id === 2 && <GlobeDemo/>}
+
+        {id === 3 && (
+          <div className=" flex gap-1 lg:gap-5 w-fit absolute =right-3 lg:-right-2">
+            <div className=" flex flex-col gap-3 lg:gap-8">
+              {['React.js', 'Next.js', 'TypeScript'].map((item) => (<span>
+                {item}
+              </span>))}
+            </div>
+
+          </div>
+        )}
+        </div>
+        </div>
+
       </div>
-      </div>
+    
     
   );
 };
