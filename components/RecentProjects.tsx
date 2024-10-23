18735ca14c7a95b92f86013dbbe3257a21fdbc69
rajeshchauhan1426 <codedemon1426@gmail.com>
2024-10-23 20:29:12 +0530

    updates

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -11,9 +11,9 @@ export const RecentProjects = () => {
         </h1>
         <div className='flex flex-wrap items-center justify-center p-4 gap-16   mt-10'>
             {projects.map (({id, title, des , img , iconLists, link}) => (
-                <div key={id} className='lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]'>
+                <div key={id} className='lg:min-h-[32.5rem] h-3/6 flex items-center justify-center sm:w-96 w-[80vw]'>
                     <PinContainer title={link} href={link}>
-                        <div className='relative flex items-center justify-center  size-8 sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] md-10'>
+                        <div className='relative flex items-center justify-center  sm:w-96 w-[80vw] overflow-hidden h-3/6 lg:h-[30vh] md-10'>
                             <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d] '>
                                 <img src='/bg.png' alt='bg-img'/> 
                              </div>
